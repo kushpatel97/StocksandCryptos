28bd4ce3063058f73bd3989c5003f08f54bad899
Kush Patel <kushp447@gmail.com>
2017-12-27 23:37:22 -0500

    Added different intervals and bootstrap

--- app.py
+++ app.py
@@ -1,55 +1,74 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import json
 import requests
 
 app = Flask(__name__)
-@app.route('/daily/symbol/<symbol>')
+@app.route('/daily/<symbol>')
 def daily(symbol):
-    API_KEY = 'O2877FVGMXZ33X94'
-    API_URL = 'https://www.alphavantage.co/query'
     function = 'TIME_SERIES_DAILY'
-    parmaters = {
-        'function': function,
-        'symbol': symbol,
-        'apikey': API_KEY
-    }
-    json_data = requests.get(API_URL, params=parmaters)
-    json_dict = json.loads(json_data.content)
+    json_dict = getResponse(symbol, function)
+    openData = getOpen(symbol,json_dict)
+    closeData = getClose(symbol, json_dict)
+    date = getOpenDate(symbol, json_dict)
+    return render_template('index.html', openData=openData, closeData=closeData, symbol=symbol, date=date)
+
+@app.route('/intraday/<symbol>')
+def intraday(symbol):
+    function = 'TIME_SERIES_INTRADAY'
+    json_dict = getResponse(symbol, function)
 
     openData = getOpen(symbol,json_dict)
     closeData = getClose(symbol, json_dict)
     date = getOpenDate(symbol, json_dict)
+    return render_template('intraday.html', openData=openData, closeData=closeData, symbol=symbol, date=date)
 
-    return render_template('index.html', openData=openData, closeData=closeData, symbol=symbol, date=date)
 
-# def getResponse(symbol, function):
-#     API_KEY = 'O2877FVGMXZ33X94'
-#     API_URL = 'https://www.alphavantage.co/query'
-#     parameters = {
-#         'function': function,
-#         'symbol': symbol,
-#         'apikey': API_KEY
-#     }
-#     json_data = requests.get(API_URL, params=parameters)
-#     json_dict = json.loads(json_data.content)
-#     return json_dict
+
+def getResponse(symbol, function):
+    API_KEY = 'O2877FVGMXZ33X94'
+    API_URL = 'https://www.alphavantage.co/query'
+    if function == 'TIME_SERIES_INTRADAY':
+        parameters = {
+            'function': function,
+            'symbol': symbol,
+            'interval': '5min',
+            'outputsize': 'compact',
+            'apikey': API_KEY
+        }
+    else:
+        parameters = {
+            'function': function,
+            'symbol': symbol,
+            'apikey': API_KEY
+        }
+
+
+    json_data = requests.get(API_URL, params=parameters)
+    json_dict = json.loads(json_data.content)
+    return json_dict
 
 # Returns float array
 def getOpen(symbol,json_dict):
+    for key in json_dict:
+        timeseries = key
     opened = []
-    for key, val in json_dict['Time Series (Daily)'].items():
+    for key, val in json_dict[timeseries].items():
         opened.append(val['1. open'])
     return list(map(float,opened))
 
 def getClose(symbol, json_dict):
+    for key in json_dict:
+        timeseries = key
     closed = []
-    for key, val in json_dict['Time Series (Daily)'].items():
+    for key, val in json_dict[timeseries].items():
         closed.append(val['4. close'])
     return list(map(float,closed))
 # Returns string array
 def getOpenDate(symbol,json_dict):
+    for key in json_dict:
+        timeseries = key
     date = []
-    for key in json_dict['Time Series (Daily)']:
+    for key in json_dict[timeseries]:
         date.append(key)
     return date
 
--- test.py
+++ test.py
@@ -2,19 +2,23 @@ import requests, json
 
 API_KEY = 'O2877FVGMXZ33X94'
 API_URL = 'https://www.alphavantage.co/query'
-function = 'TIME_SERIES_DAILY'
+function = 'TIME_SERIES_INTRADAY'
 symbol = 'AAPL'
-interval = '60min'
+interval = '5min'
 parmaters = {
     'function': function,
     'symbol': symbol,
-    # 'interval': interval,
+    'interval': interval,
+    'outputsize': 'compact',
     'apikey': API_KEY
 }
 res = requests.get(API_URL, params=parmaters)
 json_dict = json.loads(res.content)
-opened = []
-for key in json_dict['Time Series (Daily)']:
-    opened.append(key)
-# opened = ','.join(opened)
-print(opened)
+for key in json_dict:
+    ans = key
+print(key)
+# opened = []
+# for key in json_dict['Time Series (Daily)']:
+#     opened.append(key)
+# # opened = ','.join(opened)
+# print(opened)
